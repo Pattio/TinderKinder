11c91676f195c8383819c0a8d60010086e374410
Pattio <merfisku@gmail.com>
2018-10-08 22:30:09 +0100

    Initial Commit

---
+++ TinderKinder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TinderKinder
+//
+//  Created by Edvinas on 08/10/2018.
+//  Copyright © 2018 Emenity. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
